db79ca4abc097714a190661bd6b45616159054af
gustavolimasmkt-lgtm <gustavolimasmkt@gmail.com>
2026-08-10 08:30:56 -0300

    Update sw.js

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,8 +1,15 @@
-// Service Worker desinstalado
+// Desregistra e limpa tudo
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
+});
+self.addEventListener('fetch', e => {
+  // Passa tudo direto, sem interceptar nada
+  return;
 });
