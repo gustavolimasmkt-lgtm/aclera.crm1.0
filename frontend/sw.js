8c2127958f45f1a1a91686640401286d7c2914e6
gustavolimasmkt-lgtm <gustavolimasmkt@gmail.com>
2026-08-10 08:12:12 -0300

    Update sw.js

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,38 +1,8 @@
-const CACHE = 'aclera-crm-v1';
-const STATIC = [
-  '/',
-  '/manifest.json',
-  '/icons/apple-touch-icon.png',
-  '/icons/icon-192.png',
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(STATIC)).then(() => self.skipWaiting())
-  );
-});
-
+// Service Worker desinstalado
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  // Só cacheia GET, não API
-  if (e.request.method !== 'GET') return;
-  if (e.request.url.includes('/api/')) return;
-
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        // Cacheia a resposta
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
